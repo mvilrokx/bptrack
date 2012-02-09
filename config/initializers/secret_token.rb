dfa60e5bc78c59ab1796adf777e67c3d8d966b28
Mark Vilrokx <mvilrokx@gmail.com>
2012-02-08 16:58:01 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bptrack::Application.config.secret_token = 'b9e2681701986ff6742deecdbd4ff2618b2825321b7a24a14c144652d1e08d849381eb38e5576432300bed74182497d3eafaa5d62220e804b185891a41a0daee'
