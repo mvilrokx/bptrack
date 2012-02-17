dea2a22076521fa85c09c9703b58f3551612b90b
Mark Vilrokx <mvilrokx@gmail.com>
2012-02-16 16:23:00 -0800

    added config.assets.precompile += %w( polyfills.js ) to application.rb to fix issue of polifill.js not precompiling.  also removed precompiled assets cause this should fix that on Heroku

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,7 @@ module Bptrack
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     #
-    config.assets.initialize_on_precompile = false 
+    # config.assets.initialize_on_precompile = false 
+    config.assets.precompile += %w( polyfills.js )
   end
 end
